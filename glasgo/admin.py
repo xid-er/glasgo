005a2637ff3a6cf138af31496347953525caa8f4
“Yanling <2451921L@student.gla.ac.uk>
2021-03-19 22:01:04 +0800

    add parts of user authentication

--- glasgo/admin.py
+++ glasgo/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from glasgo.models import UserProfile
 # Register your models here.
---
+++ glasgo/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from glasgo.models import UserProfile
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    # describes additional properties about the particular class to which it belongs
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
