b83db2a1701739c012bcbb3ae5b0d1c96cc3a525
Simona Holubkova <2483856H@student.gla.ac.uk>
2021-04-05 13:05:34 +0200

    deleted merge notes

--- glasgo/urls.py
+++ glasgo/urls.py
@@ -14,9 +14,6 @@ urlpatterns = [
     path('user/<user_name>/edit/', views.edit_profile, name='edit_profile'),
     path('add_post/', views.add_post, name='add_post'),
     path('post/<post_number>/', views.show_post, name='show_post'),
-<<<<<<< HEAD
-=======
     path('like/', views.like, name='like_post'),
->>>>>>> f27388374590526919221e29bfcf77b5b0d288cf
 
 ]
